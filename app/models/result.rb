ec00f98d60eef9029fcb80e38bfcad1ae1285ab0
koharashota <transparent.blue69@gmail.com>
2016-05-08 05:19:38 +0900

    resultの出しわけのパラメータを変更

--- app/models/result.rb
+++ app/models/result.rb
@@ -10,22 +10,29 @@ class Result
     when 'form'
       case value
       when 1
-        '正しい方向を向いてください'
+        # '正しい方向を向いてください'
+        'Face forward'
       when 2
-        '腰が低いです'
+        # '腰が低いです'
+        'Raise the waist'
       when 3
-        '腰が高いです'
+        # '腰が高いです'
+        'Drop the waist'
       when 4
-        '膝が高いです'
+        # '膝が高いです'
+        'Bend the knee more'
       when 5
-        '膝が低いです'
+        # '膝が低いです'
+        'Raise the knee'
       end
     when 'pace'
       case value
       when 1
-        'ペースが早いです'
+        # 'ペースが早いです'
+        'Lower the pace'
       when 2
-        'ペースが遅いです'
+        # 'ペースが遅いです'
+        'Pick up the pace'
       end
     else
       ''
@@ -43,29 +50,35 @@ class Result
     end
   end
 
+  # OK: 0
+  # UP: 1
+  # DOWN: 2
+  # FRONT: 3
+  # BACK: 4
   def gesture
     case type
     when 'form'
       case value
       when 1
-        1
+        3
       when 2
         1
       when 3
-        1
+        2
       when 4
-        1
+        2
       when 5
         1
       end
     when 'pace'
       case value
       when 1
-        1
+        2
       when 2
         1
       end
     else
+      0
     end
   end
 end
