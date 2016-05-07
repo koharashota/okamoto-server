e764c1818cdb588995d329daa8986b7ce96f2340
koharashota <transparent.blue69@gmail.com>
2016-05-08 00:14:19 +0900

    SegmentsControllerが消えていたので追加

---
+++ app/controllers/api/kinect/segments_controller.rb
@@ -0,0 +1,19 @@
+class Api::Kinect::SegmentsController < Api::ApiController
+  def create
+    if new_segment.save
+    end
+  end
+
+  private
+
+  def segment_params
+    # coordinates [ [x0, y0], [x1, y1] ]
+    #params.require(:segment).permit( frames_attributes: [ :coordinates ] )
+    #params.require(:segment).permit( frames_attributes: [ coordinates: [] ] )
+    params.require(:segment)
+  end
+
+  def new_segment
+    @new_segment ||= Segment.new(segment_params)
+  end
+end
